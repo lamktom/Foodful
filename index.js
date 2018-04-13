7f887d5bab55c1155ead15544fa9a19cacdea5b6
lamktom <lamktom@yahoo.com>
2018-04-12 17:12:44 -0700

    started js

---
+++ index.js
@@ -0,0 +1,18 @@
+// consts for keys, endpoints
+const FOOD2FORK_URL = "http://food2fork.com/api/search"; 
+const FATSECRET_url = "https://platform.fatsecret.com/"; 
+
+const f2f_key = "2e51c7a2be396263cadc671eb7de55d1";
+const fs_key = "69d01778942a4e9ca437e7d97444f98f"; 
+
+
+// when user enters ingredients and clicks submit 
+function watchSubmit() {
+	
+}
+
+// show results 
+function renderResults() {
+	
+}
+
