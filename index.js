399c2a240ecb1e6d136e3fb62ae3833591a3574e
Tom Lam <lamktom@yahoo.com>
2018-04-22 19:03:24 -0700

    Updated

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-fatsecret.setContainer("my_container");
-fatsecret.setCanvas("home");
+// fatsecret.setContainer("my_container");
+// fatsecret.setCanvas("home");
 
 // consts for endpoints keys
 const YUMMLY_URL = "http://api.yummly.com/v1/api/recipes"; 
-const FATSECRET_URL = "https://platform.fatsecret.com/"; 
+// const FATSECRET_URL = "https://platform.fatsecret.com/"; 
 
 const y_app_id = "23406cce"
 const y_key = "edfd201c5bce9ccf028432345e2e7e23";
-const fs_key = "69d01778942a4e9ca437e7d97444f98f"; 
+// const fs_key = "69d01778942a4e9ca437e7d97444f98f"; 
 
 //get data from api 
 function getApiData(searchTerm, callback) {
 	const settings = {
-		// parameters
 		url: YUMMLY_URL, 
 		data: {
 			// part: 'recipes',
 			_app_key: y_key, 
 			_app_id: y_app_id,
 			requirePictures: true,  
-			q: searchTerm 
+			q: searchTerm
 		}, 
 		dataType: 'jsonp', 
 		type: 'GET',
-		success: callback
+		success: callback,
 	};
 	$.ajax(settings); 
 }
@@ -33,7 +32,7 @@ function renderResult(result) {
 	console.log(result);
 	return `
 		<div> 
-			<a href="http://food2fork.com/view/${result.recipe_id}"><img class="js-image" src="${result.image_url}" alt="Recipe Thumbnail"></a>
+			<a href="https://www.yummly.com/recipes/${result.id}"><img class="js-image" src="${result.smallImageUrls}" alt="Recipe Thumbnail"></a>
 		</div>
   	`; 
 }
